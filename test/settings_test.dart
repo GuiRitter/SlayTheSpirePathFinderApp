76ac283580e8a6ebe25b824cbc77bf467072cc37
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-07-03 13:55:24 -0300

    fixed regex

--- test/settings_test.dart
+++ test/settings_test.dart
@@ -119,9 +119,9 @@ void main() {
   );
 
   test(
-    'exiting',
+    'all matches',
     () async {
-      final matchList = Settings.exitingFloorRegex.allMatches(
+      final matchList = Settings.floorRegex.allMatches(
         "N-U0 U0-M1\nM1-T9 T9-R10 R10-E19\nE19-L100 L100-B\n",
       );
 
@@ -129,6 +129,7 @@ void main() {
         matchList.length,
         7,
       );
+
       expect(
         matchList
             .elementAt(
@@ -165,6 +166,40 @@ void main() {
       expect(
         matchList
             .elementAt(
+              0,
+            )
+            .namedGroup(
+              "enteringBoss",
+            ),
+        null,
+        reason: "0 - enteringBoss",
+      );
+      expect(
+        matchList
+            .elementAt(
+              0,
+            )
+            .namedGroup(
+              "enteringFloor",
+            ),
+        "U",
+        reason: "0 - enteringFloor",
+      );
+      expect(
+        matchList
+            .elementAt(
+              0,
+            )
+            .namedGroup(
+              "enteringNumber",
+            ),
+        "0",
+        reason: "0 - enteringNumber",
+      );
+
+      expect(
+        matchList
+            .elementAt(
               1,
             )
             .namedGroup(
@@ -195,6 +230,40 @@ void main() {
         "0",
         reason: "1 - exitingNumber",
       );
+      expect(
+        matchList
+            .elementAt(
+              1,
+            )
+            .namedGroup(
+              "enteringBoss",
+            ),
+        null,
+        reason: "1 - enteringBoss",
+      );
+      expect(
+        matchList
+            .elementAt(
+              1,
+            )
+            .namedGroup(
+              "enteringFloor",
+            ),
+        "M",
+        reason: "1 - enteringFloor",
+      );
+      expect(
+        matchList
+            .elementAt(
+              1,
+            )
+            .namedGroup(
+              "enteringNumber",
+            ),
+        "1",
+        reason: "1 - enteringNumber",
+      );
+
       expect(
         matchList
             .elementAt(
@@ -231,6 +300,40 @@ void main() {
       expect(
         matchList
             .elementAt(
+              2,
+            )
+            .namedGroup(
+              "enteringBoss",
+            ),
+        null,
+        reason: "2 - enteringBoss",
+      );
+      expect(
+        matchList
+            .elementAt(
+              2,
+            )
+            .namedGroup(
+              "enteringFloor",
+            ),
+        "T",
+        reason: "2 - enteringFloor",
+      );
+      expect(
+        matchList
+            .elementAt(
+              2,
+            )
+            .namedGroup(
+              "enteringNumber",
+            ),
+        "9",
+        reason: "2 - enteringNumber",
+      );
+
+      expect(
+        matchList
+            .elementAt(
               3,
             )
             .namedGroup(
@@ -261,6 +364,40 @@ void main() {
         "9",
         reason: "3 - exitingNumber",
       );
+      expect(
+        matchList
+            .elementAt(
+              3,
+            )
+            .namedGroup(
+              "enteringBoss",
+            ),
+        null,
+        reason: "3 - enteringBoss",
+      );
+      expect(
+        matchList
+            .elementAt(
+              3,
+            )
+            .namedGroup(
+              "enteringFloor",
+            ),
+        "R",
+        reason: "3 - enteringFloor",
+      );
+      expect(
+        matchList
+            .elementAt(
+              3,
+            )
+            .namedGroup(
+              "enteringNumber",
+            ),
+        "10",
+        reason: "3 - enteringNumber",
+      );
+
       expect(
         matchList
             .elementAt(
@@ -297,221 +434,6 @@ void main() {
       expect(
         matchList
             .elementAt(
-              5,
-            )
-            .namedGroup(
-              "exitingNeow",
-            ),
-        null,
-        reason: "5 - exitingNeow",
-      );
-      expect(
-        matchList
-            .elementAt(
-              5,
-            )
-            .namedGroup(
-              "exitingFloor",
-            ),
-        "E",
-        reason: "5 - exitingFloor",
-      );
-      expect(
-        matchList
-            .elementAt(
-              5,
-            )
-            .namedGroup(
-              "exitingNumber",
-            ),
-        "19",
-        reason: "5 - exitingNumber",
-      );
-      expect(
-        matchList
-            .elementAt(
-              6,
-            )
-            .namedGroup(
-              "exitingNeow",
-            ),
-        null,
-        reason: "6 - exitingNeow",
-      );
-      expect(
-        matchList
-            .elementAt(
-              6,
-            )
-            .namedGroup(
-              "exitingFloor",
-            ),
-        "L",
-        reason: "6 - exitingFloor",
-      );
-      expect(
-        matchList
-            .elementAt(
-              6,
-            )
-            .namedGroup(
-              "exitingNumber",
-            ),
-        "100",
-        reason: "6 - exitingNumber",
-      );
-    },
-  );
-
-  test(
-    'entering',
-    () async {
-      final matchList = Settings.enteringFloorRegex.allMatches(
-        "N-U0 U0-M1\nM1-T9 T9-R10 R10-E19\nE19-L100 L100-B\n".replaceAll(
-          Settings.exitingFloorRegex,
-          "",
-        ),
-      );
-
-      expect(
-        matchList.length,
-        7,
-      );
-      expect(
-        matchList
-            .elementAt(
-              0,
-            )
-            .namedGroup(
-              "enteringBoss",
-            ),
-        null,
-        reason: "0 - enteringBoss",
-      );
-      expect(
-        matchList
-            .elementAt(
-              0,
-            )
-            .namedGroup(
-              "enteringFloor",
-            ),
-        "U",
-        reason: "0 - enteringFloor",
-      );
-      expect(
-        matchList
-            .elementAt(
-              0,
-            )
-            .namedGroup(
-              "enteringNumber",
-            ),
-        "0",
-        reason: "0 - enteringNumber",
-      );
-      expect(
-        matchList
-            .elementAt(
-              1,
-            )
-            .namedGroup(
-              "enteringBoss",
-            ),
-        null,
-        reason: "1 - enteringBoss",
-      );
-      expect(
-        matchList
-            .elementAt(
-              1,
-            )
-            .namedGroup(
-              "enteringFloor",
-            ),
-        "M",
-        reason: "1 - enteringFloor",
-      );
-      expect(
-        matchList
-            .elementAt(
-              1,
-            )
-            .namedGroup(
-              "enteringNumber",
-            ),
-        "1",
-        reason: "1 - enteringNumber",
-      );
-      expect(
-        matchList
-            .elementAt(
-              2,
-            )
-            .namedGroup(
-              "enteringBoss",
-            ),
-        null,
-        reason: "2 - enteringBoss",
-      );
-      expect(
-        matchList
-            .elementAt(
-              2,
-            )
-            .namedGroup(
-              "enteringFloor",
-            ),
-        "T",
-        reason: "2 - enteringFloor",
-      );
-      expect(
-        matchList
-            .elementAt(
-              2,
-            )
-            .namedGroup(
-              "enteringNumber",
-            ),
-        "9",
-        reason: "2 - enteringNumber",
-      );
-      expect(
-        matchList
-            .elementAt(
-              3,
-            )
-            .namedGroup(
-              "enteringBoss",
-            ),
-        null,
-        reason: "3 - enteringBoss",
-      );
-      expect(
-        matchList
-            .elementAt(
-              3,
-            )
-            .namedGroup(
-              "enteringFloor",
-            ),
-        "R",
-        reason: "3 - enteringFloor",
-      );
-      expect(
-        matchList
-            .elementAt(
-              3,
-            )
-            .namedGroup(
-              "enteringNumber",
-            ),
-        "10",
-        reason: "3 - enteringNumber",
-      );
-      expect(
-        matchList
-            .elementAt(
               4,
             )
             .namedGroup(
@@ -541,6 +463,40 @@ void main() {
             ),
         "19",
         reason: "4 - enteringNumber",
+      );
+
+      expect(
+        matchList
+            .elementAt(
+              5,
+            )
+            .namedGroup(
+              "exitingNeow",
+            ),
+        null,
+        reason: "5 - exitingNeow",
+      );
+      expect(
+        matchList
+            .elementAt(
+              5,
+            )
+            .namedGroup(
+              "exitingFloor",
+            ),
+        "E",
+        reason: "5 - exitingFloor",
+      );
+      expect(
+        matchList
+            .elementAt(
+              5,
+            )
+            .namedGroup(
+              "exitingNumber",
+            ),
+        "19",
+        reason: "5 - exitingNumber",
       );
       expect(
         matchList
@@ -575,6 +531,40 @@ void main() {
         "100",
         reason: "5 - enteringNumber",
       );
+
+      expect(
+        matchList
+            .elementAt(
+              6,
+            )
+            .namedGroup(
+              "exitingNeow",
+            ),
+        null,
+        reason: "6 - exitingNeow",
+      );
+      expect(
+        matchList
+            .elementAt(
+              6,
+            )
+            .namedGroup(
+              "exitingFloor",
+            ),
+        "L",
+        reason: "6 - exitingFloor",
+      );
+      expect(
+        matchList
+            .elementAt(
+              6,
+            )
+            .namedGroup(
+              "exitingNumber",
+            ),
+        "100",
+        reason: "6 - exitingNumber",
+      );
       expect(
         matchList
             .elementAt(
@@ -607,6 +597,20 @@ void main() {
             ),
         null,
         reason: "6 - enteringNumber",
+      );
+    },
+  );
+
+  test(
+    'allMatches does not fail despite malformatted input',
+    () async {
+      final matchList = Settings.floorRegex.allMatches(
+        "N-U0 U0-M1\nM1-T9 T9R10 R10-E19\nE19-L100 L100-B\n",
+      );
+
+      expect(
+        matchList.length,
+        6,
       );
     },
   );
